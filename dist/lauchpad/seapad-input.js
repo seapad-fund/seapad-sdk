b3165be3480a6442f2011ec8755dc672f22c296b
vxhuy <vxhuy17694@gmail.com>
2023-03-30 10:42:36 +0700

    update

--- dist/lauchpad/seapad-input.js
+++ dist/lauchpad/seapad-input.js
@@ -155,7 +155,6 @@ class SeaPadInput extends seapad_func_1.SeaPadFunc {
             ],
             typeArguments: [types.COIN],
         });
-        tx.splitCoins(tx.gas, [tx.pure(args.amount)]);
         return tx;
     }
     endFundRaising(types, args, optionTx, gasBudget) {
