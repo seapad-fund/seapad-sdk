66df19d57282719fd797a7e556dc17af158fdd14
Neo Keanu <avite.tran+web3lab@gmail.com>
2023-04-20 23:25:03 +0700

    add  duration_unstake_time_ms

--- src/stakepool/seapad-sp-adapter.ts
+++ src/stakepool/seapad-sp-adapter.ts
@@ -1,131 +1,178 @@
 import {
-    RawSigner,
-    JsonRpcProvider,
-    SuiTransactionBlockResponse,
+  RawSigner,
+  JsonRpcProvider,
+  SuiTransactionBlockResponse,
 } from '@mysten/sui.js';
 import { SeaPadStakePoolFunc } from './seapad-sp-func';
 import { SeaPadStakePoolInput } from './seapad-sp-input';
 import { GasBudget, OptionTx } from '../common';
 
 export class SeaPadStakePoolAdapter extends SeaPadStakePoolFunc<
-    Promise<SuiTransactionBlockResponse>
+  Promise<SuiTransactionBlockResponse>
 > {
+  _suiProvider: JsonRpcProvider;
+  _seaPadStakePoolInput: SeaPadStakePoolInput;
+  _signer: RawSigner;
 
-    _suiProvider: JsonRpcProvider;
-    _seaPadStakePoolInput: SeaPadStakePoolInput;
-    _signer: RawSigner;
+  constructor(signer: RawSigner, packageObjectId: string, module: string) {
+    super();
+    this._seaPadStakePoolInput = new SeaPadStakePoolInput(
+      packageObjectId,
+      module,
+    );
+    this._signer = signer;
+    this._suiProvider = signer.provider;
+  }
 
-    constructor(signer: RawSigner, packageObjectId: string, module: string) {
-        super();
-        this._seaPadStakePoolInput = new SeaPadStakePoolInput(packageObjectId, module);
-        this._signer = signer;
-        this._suiProvider = signer.provider;
+  _getOptionTx(optionTx: OptionTx): OptionTx {
+    if (optionTx) {
+      return optionTx;
     }
+    return {
+      options: {
+        showInput: true,
+        showEffects: true,
+        showEvents: true,
+        showObjectChanges: false,
+        showBalanceChanges: false,
+      },
+      requestType: 'WaitForEffectsCert',
+    };
+  }
 
-    _getOptionTx(optionTx: OptionTx): OptionTx {
-        if (optionTx) {
-            return optionTx;
-        }
-        return {
-            options: {
-                showInput: true,
-                showEffects: true,
-                showEvents: true,
-                showObjectChanges: false,
-                showBalanceChanges: false,
-            },
-            requestType: 'WaitForEffectsCert',
-        };
-    }
-
-    async registerPool(types: { S: string; R: string; }, args: { rewards: string; duration: string; global_config: string; decimalS: number; decimalR: number; }, optionTx?: OptionTx, gasBudget?: GasBudget | undefined): Promise<SuiTransactionBlockResponse>  {
-        return await this._signer.signAndExecuteTransactionBlock({
-            transactionBlock: this._seaPadStakePoolInput.registerPool(
-                types,
-                args,
-                optionTx,
-                gasBudget,
-            ),
-            ...this._getOptionTx(optionTx),
-        });
-    }
-    async stake(types: { S: string; R: string; }, args: { pool: string; coins: string; global_config: string; }, optionTx?: OptionTx, gasBudget?: GasBudget | undefined): Promise<SuiTransactionBlockResponse>  {
-        return await this._signer.signAndExecuteTransactionBlock({
-            transactionBlock: this._seaPadStakePoolInput.stake(
-                types,
-                args,
-                optionTx,
-                gasBudget,
-            ),
-            ...this._getOptionTx(optionTx),
-        });
-    }
-    async unstake(types: { S: string; R: string; }, args: { pool: string; stake_amount: string; global_config: string; }, optionTx?: OptionTx, gasBudget?: GasBudget | undefined): Promise<SuiTransactionBlockResponse>  {
-        return await this._signer.signAndExecuteTransactionBlock({
-            transactionBlock: this._seaPadStakePoolInput.unstake(
-                types,
-                args,
-                optionTx,
-                gasBudget,
-            ),
-            ...this._getOptionTx(optionTx),
-        });
-    }
-    async harvest(types: { S: string; R: string; }, args: { pool: string; global_config: string; }, optionTx?: OptionTx, gasBudget?: GasBudget | undefined): Promise<SuiTransactionBlockResponse>  {
-        return await this._signer.signAndExecuteTransactionBlock({
-            transactionBlock: this._seaPadStakePoolInput.harvest(
-                types,
-                args,
-                optionTx,
-                gasBudget,
-            ),
-            ...this._getOptionTx(optionTx),
-        });
-    }
-    async depositRewardCoins(types: { S: string; R: string; }, args: { pool: string; reward_coins: string; global_config: string; }, optionTx?: OptionTx, gasBudget?: GasBudget | undefined): Promise<SuiTransactionBlockResponse>  {
-        return await this._signer.signAndExecuteTransactionBlock({
-            transactionBlock: this._seaPadStakePoolInput.depositRewardCoins(
-                types,
-                args,
-                optionTx,
-                gasBudget,
-            ),
-            ...this._getOptionTx(optionTx),
-        });
-    }
-    async enableEmergency(types: { S: string; R: string; }, args: { pool: string; global_config: string; }, optionTx?: OptionTx, gasBudget?: GasBudget | undefined): Promise<SuiTransactionBlockResponse>  {
-        return await this._signer.signAndExecuteTransactionBlock({
-            transactionBlock: this._seaPadStakePoolInput.enableEmergency(
-                types,
-                args,
-                optionTx,
-                gasBudget,
-            ),
-            ...this._getOptionTx(optionTx),
-        });
-    }
-    async emergencyUnstake(types: { S: string; R: string; }, args: { pool: string; amount: string; global_config: string; }, optionTx?: OptionTx, gasBudget?: GasBudget | undefined): Promise<SuiTransactionBlockResponse>  {
-        return await this._signer.signAndExecuteTransactionBlock({
-            transactionBlock: this._seaPadStakePoolInput.emergencyUnstake(
-                types,
-                args,
-                optionTx,
-                gasBudget,
-            ),
-            ...this._getOptionTx(optionTx),
-        });
-    }
-    async withdrawRewardToTreasury(types: { S: string; R: string; }, args: { pool: string; amount: string; global_config: string; }, optionTx?: OptionTx, gasBudget?: GasBudget | undefined): Promise<SuiTransactionBlockResponse>  {
-        return await this._signer.signAndExecuteTransactionBlock({
-            transactionBlock: this._seaPadStakePoolInput.withdrawRewardToTreasury(
-                types,
-                args,
-                optionTx,
-                gasBudget,
-            ),
-            ...this._getOptionTx(optionTx),
-        });
-    }
-
-
+  async registerPool(
+    types: { S: string; R: string },
+    args: {
+      rewards: string;
+      duration: string;
+      global_config: string;
+      decimalS: number;
+      decimalR: number;
+      duration_unstake_time_ms: number;
+    },
+    optionTx?: OptionTx,
+    gasBudget?: GasBudget | undefined,
+  ): Promise<SuiTransactionBlockResponse> {
+    return await this._signer.signAndExecuteTransactionBlock({
+      transactionBlock: this._seaPadStakePoolInput.registerPool(
+        types,
+        args,
+        optionTx,
+        gasBudget,
+      ),
+      ...this._getOptionTx(optionTx),
+    });
+  }
+  async stake(
+    types: { S: string; R: string },
+    args: { pool: string; coins: string; global_config: string },
+    optionTx?: OptionTx,
+    gasBudget?: GasBudget | undefined,
+  ): Promise<SuiTransactionBlockResponse> {
+    return await this._signer.signAndExecuteTransactionBlock({
+      transactionBlock: this._seaPadStakePoolInput.stake(
+        types,
+        args,
+        optionTx,
+        gasBudget,
+      ),
+      ...this._getOptionTx(optionTx),
+    });
+  }
+  async unstake(
+    types: { S: string; R: string },
+    args: { pool: string; stake_amount: string; global_config: string },
+    optionTx?: OptionTx,
+    gasBudget?: GasBudget | undefined,
+  ): Promise<SuiTransactionBlockResponse> {
+    return await this._signer.signAndExecuteTransactionBlock({
+      transactionBlock: this._seaPadStakePoolInput.unstake(
+        types,
+        args,
+        optionTx,
+        gasBudget,
+      ),
+      ...this._getOptionTx(optionTx),
+    });
+  }
+  async harvest(
+    types: { S: string; R: string },
+    args: { pool: string; global_config: string },
+    optionTx?: OptionTx,
+    gasBudget?: GasBudget | undefined,
+  ): Promise<SuiTransactionBlockResponse> {
+    return await this._signer.signAndExecuteTransactionBlock({
+      transactionBlock: this._seaPadStakePoolInput.harvest(
+        types,
+        args,
+        optionTx,
+        gasBudget,
+      ),
+      ...this._getOptionTx(optionTx),
+    });
+  }
+  async depositRewardCoins(
+    types: { S: string; R: string },
+    args: { pool: string; reward_coins: string; global_config: string },
+    optionTx?: OptionTx,
+    gasBudget?: GasBudget | undefined,
+  ): Promise<SuiTransactionBlockResponse> {
+    return await this._signer.signAndExecuteTransactionBlock({
+      transactionBlock: this._seaPadStakePoolInput.depositRewardCoins(
+        types,
+        args,
+        optionTx,
+        gasBudget,
+      ),
+      ...this._getOptionTx(optionTx),
+    });
+  }
+  async enableEmergency(
+    types: { S: string; R: string },
+    args: { pool: string; global_config: string },
+    optionTx?: OptionTx,
+    gasBudget?: GasBudget | undefined,
+  ): Promise<SuiTransactionBlockResponse> {
+    return await this._signer.signAndExecuteTransactionBlock({
+      transactionBlock: this._seaPadStakePoolInput.enableEmergency(
+        types,
+        args,
+        optionTx,
+        gasBudget,
+      ),
+      ...this._getOptionTx(optionTx),
+    });
+  }
+  async emergencyUnstake(
+    types: { S: string; R: string },
+    args: { pool: string; amount: string; global_config: string },
+    optionTx?: OptionTx,
+    gasBudget?: GasBudget | undefined,
+  ): Promise<SuiTransactionBlockResponse> {
+    return await this._signer.signAndExecuteTransactionBlock({
+      transactionBlock: this._seaPadStakePoolInput.emergencyUnstake(
+        types,
+        args,
+        optionTx,
+        gasBudget,
+      ),
+      ...this._getOptionTx(optionTx),
+    });
+  }
+  async withdrawRewardToTreasury(
+    types: { S: string; R: string },
+    args: { pool: string; amount: string; global_config: string },
+    optionTx?: OptionTx,
+    gasBudget?: GasBudget | undefined,
+  ): Promise<SuiTransactionBlockResponse> {
+    return await this._signer.signAndExecuteTransactionBlock({
+      transactionBlock: this._seaPadStakePoolInput.withdrawRewardToTreasury(
+        types,
+        args,
+        optionTx,
+        gasBudget,
+      ),
+      ...this._getOptionTx(optionTx),
+    });
+  }
 }
--- src/stakepool/seapad-sp-wallet-adapter.ts
+++ src/stakepool/seapad-sp-wallet-adapter.ts
@@ -3,129 +3,179 @@ import { SeaPadStakePoolInput } from './seapad-sp-input';
 import { WalletContextState } from '@suiet/wallet-kit';
 import { TransactionBlock } from '@mysten/sui.js';
 import {
-    SuiSignAndExecuteTransactionBlockInput,
-    SuiSignAndExecuteTransactionBlockOutput,
+  SuiSignAndExecuteTransactionBlockInput,
+  SuiSignAndExecuteTransactionBlockOutput,
 } from '@mysten/wallet-standard';
 import { GasBudget, OptionTx } from '../common';
 
-export class SeapadWalletSpAdapter extends SeaPadStakePoolFunc<Promise<SuiSignAndExecuteTransactionBlockOutput>> {
+export class SeapadWalletSpAdapter extends SeaPadStakePoolFunc<
+  Promise<SuiSignAndExecuteTransactionBlockOutput>
+> {
+  _walletContextState: WalletContextState;
+  _seaPadStakePoolInput: SeaPadStakePoolInput;
 
-    _walletContextState: WalletContextState;
-    _seaPadStakePoolInput: SeaPadStakePoolInput;
+  constructor(
+    walletContextState: WalletContextState,
+    packageObjectId: string,
+    module: string,
+  ) {
+    super();
+    this._seaPadStakePoolInput = new SeaPadStakePoolInput(
+      packageObjectId,
+      module,
+    );
+    this._walletContextState = walletContextState;
+  }
 
-    constructor(
-        walletContextState: WalletContextState,
-        packageObjectId: string,
-        module: string,
-    ) {
-        super();
-        this._seaPadStakePoolInput = new SeaPadStakePoolInput(packageObjectId, module);
-        this._walletContextState = walletContextState;
-    }
+  async registerPool(
+    types: { S: string; R: string },
+    args: {
+      rewards: string;
+      duration: string;
+      global_config: string;
+      decimalS: number;
+      decimalR: number;
+      duration_unstake_time_ms: number;
+    },
+    optionTx?: OptionTx,
+    gasBudget?: GasBudget | undefined,
+  ): Promise<SuiSignAndExecuteTransactionBlockOutput> {
+    const message = this._seaPadStakePoolInput.registerPool(
+      types,
+      args,
+      optionTx,
+      gasBudget,
+    );
+    return await this._walletContextState.signAndExecuteTransactionBlock(
+      this.buildTx(message),
+    );
+  }
+  async stake(
+    types: { S: string; R: string },
+    args: { pool: string; coins: string; global_config: string },
+    optionTx?: OptionTx,
+    gasBudget?: GasBudget | undefined,
+  ): Promise<SuiSignAndExecuteTransactionBlockOutput> {
+    const message = this._seaPadStakePoolInput.stake(
+      types,
+      args,
+      optionTx,
+      gasBudget,
+    );
+    return await this._walletContextState.signAndExecuteTransactionBlock(
+      this.buildTx(message),
+    );
+  }
+  async unstake(
+    types: { S: string; R: string },
+    args: { pool: string; stake_amount: string; global_config: string },
+    optionTx?: OptionTx,
+    gasBudget?: GasBudget | undefined,
+  ): Promise<SuiSignAndExecuteTransactionBlockOutput> {
+    const message = this._seaPadStakePoolInput.unstake(
+      types,
+      args,
+      optionTx,
+      gasBudget,
+    );
+    return await this._walletContextState.signAndExecuteTransactionBlock(
+      this.buildTx(message),
+    );
+  }
+  async harvest(
+    types: { S: string; R: string },
+    args: { pool: string; global_config: string },
+    optionTx?: OptionTx,
+    gasBudget?: GasBudget | undefined,
+  ): Promise<SuiSignAndExecuteTransactionBlockOutput> {
+    const message = this._seaPadStakePoolInput.harvest(
+      types,
+      args,
+      optionTx,
+      gasBudget,
+    );
+    return await this._walletContextState.signAndExecuteTransactionBlock(
+      this.buildTx(message),
+    );
+  }
+  async depositRewardCoins(
+    types: { S: string; R: string },
+    args: { pool: string; reward_coins: string; global_config: string },
+    optionTx?: OptionTx,
+    gasBudget?: GasBudget | undefined,
+  ): Promise<SuiSignAndExecuteTransactionBlockOutput> {
+    const message = this._seaPadStakePoolInput.depositRewardCoins(
+      types,
+      args,
+      optionTx,
+      gasBudget,
+    );
+    return await this._walletContextState.signAndExecuteTransactionBlock(
+      this.buildTx(message),
+    );
+  }
+  async enableEmergency(
+    types: { S: string; R: string },
+    args: { pool: string; global_config: string },
+    optionTx?: OptionTx,
+    gasBudget?: GasBudget | undefined,
+  ): Promise<SuiSignAndExecuteTransactionBlockOutput> {
+    const message = this._seaPadStakePoolInput.enableEmergency(
+      types,
+      args,
+      optionTx,
+      gasBudget,
+    );
+    return await this._walletContextState.signAndExecuteTransactionBlock(
+      this.buildTx(message),
+    );
+  }
+  async emergencyUnstake(
+    types: { S: string; R: string },
+    args: { pool: string; amount: string; global_config: string },
+    optionTx?: OptionTx,
+    gasBudget?: GasBudget | undefined,
+  ): Promise<SuiSignAndExecuteTransactionBlockOutput> {
+    const message = this._seaPadStakePoolInput.emergencyUnstake(
+      types,
+      args,
+      optionTx,
+      gasBudget,
+    );
+    return await this._walletContextState.signAndExecuteTransactionBlock(
+      this.buildTx(message),
+    );
+  }
+  async withdrawRewardToTreasury(
+    types: { S: string; R: string },
+    args: { pool: string; amount: string; global_config: string },
+    optionTx?: OptionTx,
+    gasBudget?: GasBudget | undefined,
+  ): Promise<SuiSignAndExecuteTransactionBlockOutput> {
+    const message = this._seaPadStakePoolInput.withdrawRewardToTreasury(
+      types,
+      args,
+      optionTx,
+      gasBudget,
+    );
+    return await this._walletContextState.signAndExecuteTransactionBlock(
+      this.buildTx(message),
+    );
+  }
 
-
-    async registerPool(types: { S: string; R: string; }, args: { rewards: string; duration: string; global_config: string; decimalS: number; decimalR: number; }, optionTx?: OptionTx, gasBudget?: GasBudget | undefined): Promise<SuiSignAndExecuteTransactionBlockOutput> {
-        const message = this._seaPadStakePoolInput.registerPool(
-      types,
-      args,
-      optionTx,
-      gasBudget,
-    );
-    return await this._walletContextState.signAndExecuteTransactionBlock(
-      this.buildTx(message),
-    );
-    }
-    async stake(types: { S: string; R: string; }, args: { pool: string; coins: string; global_config: string; }, optionTx?: OptionTx, gasBudget?: GasBudget | undefined): Promise<SuiSignAndExecuteTransactionBlockOutput> {
-        const message = this._seaPadStakePoolInput.stake(
-      types,
-      args,
-      optionTx,
-      gasBudget,
-    );
-    return await this._walletContextState.signAndExecuteTransactionBlock(
-      this.buildTx(message),
-    );
-    }
-    async unstake(types: { S: string; R: string; }, args: { pool: string; stake_amount: string; global_config: string; }, optionTx?: OptionTx, gasBudget?: GasBudget | undefined): Promise<SuiSignAndExecuteTransactionBlockOutput> {
-        const message = this._seaPadStakePoolInput.unstake(
-      types,
-      args,
-      optionTx,
-      gasBudget,
-    );
-    return await this._walletContextState.signAndExecuteTransactionBlock(
-      this.buildTx(message),
-    );
-    }
-    async harvest(types: { S: string; R: string; }, args: { pool: string; global_config: string; }, optionTx?: OptionTx, gasBudget?: GasBudget | undefined): Promise<SuiSignAndExecuteTransactionBlockOutput> {
-        const message = this._seaPadStakePoolInput.harvest(
-      types,
-      args,
-      optionTx,
-      gasBudget,
-    );
-    return await this._walletContextState.signAndExecuteTransactionBlock(
-      this.buildTx(message),
-    );
-    }
-    async depositRewardCoins(types: { S: string; R: string; }, args: { pool: string; reward_coins: string; global_config: string; }, optionTx?: OptionTx, gasBudget?: GasBudget | undefined): Promise<SuiSignAndExecuteTransactionBlockOutput> {
-        const message = this._seaPadStakePoolInput.depositRewardCoins(
-      types,
-      args,
-      optionTx,
-      gasBudget,
-    );
-    return await this._walletContextState.signAndExecuteTransactionBlock(
-      this.buildTx(message),
-    );
-    }
-    async enableEmergency(types: { S: string; R: string; }, args: { pool: string; global_config: string; }, optionTx?: OptionTx, gasBudget?: GasBudget | undefined): Promise<SuiSignAndExecuteTransactionBlockOutput> {
-        const message = this._seaPadStakePoolInput.enableEmergency(
-      types,
-      args,
-      optionTx,
-      gasBudget,
-    );
-    return await this._walletContextState.signAndExecuteTransactionBlock(
-      this.buildTx(message),
-    );
-    }
-    async emergencyUnstake(types: { S: string; R: string; }, args: { pool: string; amount: string; global_config: string; }, optionTx?: OptionTx, gasBudget?: GasBudget | undefined): Promise<SuiSignAndExecuteTransactionBlockOutput> {
-        const message = this._seaPadStakePoolInput.emergencyUnstake(
-      types,
-      args,
-      optionTx,
-      gasBudget,
-    );
-    return await this._walletContextState.signAndExecuteTransactionBlock(
-      this.buildTx(message),
-    );
-    }
-    async withdrawRewardToTreasury(types: { S: string; R: string; }, args: { pool: string; amount: string; global_config: string; }, optionTx?: OptionTx, gasBudget?: GasBudget | undefined): Promise<SuiSignAndExecuteTransactionBlockOutput> {
-        const message = this._seaPadStakePoolInput.withdrawRewardToTreasury(
-      types,
-      args,
-      optionTx,
-      gasBudget,
-    );
-    return await this._walletContextState.signAndExecuteTransactionBlock(
-      this.buildTx(message),
-    );
-    }
-
-    buildTx(
-        message: TransactionBlock,
-    ): Omit<SuiSignAndExecuteTransactionBlockInput, 'account' | 'chain'> {
-        return {
-            transactionBlock: message,
-            options: {
-                showInput: false,
-                showEffects: true,
-                showEvents: true,
-                showObjectChanges: false,
-                showBalanceChanges: false,
-            },
-            requestType: 'WaitForEffectsCert',
-        };
-    }
+  buildTx(
+    message: TransactionBlock,
+  ): Omit<SuiSignAndExecuteTransactionBlockInput, 'account' | 'chain'> {
+    return {
+      transactionBlock: message,
+      options: {
+        showInput: false,
+        showEffects: true,
+        showEvents: true,
+        showObjectChanges: false,
+        showBalanceChanges: false,
+      },
+      requestType: 'WaitForEffectsCert',
+    };
+  }
 }
